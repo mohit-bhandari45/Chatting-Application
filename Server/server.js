25a8a3ed3289ac7d0ce3989e81703a12e653ee75
mohit-bhandari45 <mohitbhandari852@gmail.com>
2024-03-27 18:33:00 +0530

    Changed and added some files

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const cors=require("cors")
+const mongoose=require("mongoose")
+const app = express()
+require("dotenv").config();
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true,
+}).then(()=>{
+  console.log("DB connection Successful");
+}).catch((err)=>{
+  console.log(err.message);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
